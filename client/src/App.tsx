317804ae95ddaebe2ced6705dad50eba12a26c60
yipepepep <oilshayforever@gmail.com>
2025-05-13 11:34:10 -0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,12 +6,14 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import TrainingApp from "@/pages/TrainingApp";
 
-function Router() {
+function AppRouter() {
   return (
-    <Switch>
-      <Route path="/" component={TrainingApp} />
-      <Route component={NotFound} />
-    </Switch>
+    <Router base={process.env.BASE_URL || "/"}>
+      <Switch>
+        <Route path="/" component={TrainingApp} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
@@ -20,7 +22,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <AppRouter />
       </TooltipProvider>
     </QueryClientProvider>
   );
